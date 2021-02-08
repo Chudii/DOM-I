fd1d99f22804e83663521c2c3b704118a00d8bd9
Chudi Ibida <chudiibida3@gmail.com>
2021-02-08 17:59:47 -0500

    chudi ibida

--- js/index.js
+++ js/index.js
@@ -37,6 +37,85 @@ const siteContent = {
   },
 };
 
+//body > div > header > nav > a:nth-child(1)
+
+//nav
+const navEl = document.querySelector('nav');
+let navServices = document.querySelector('a');
+navServices.textContent = siteContent["nav"]["nav-item-1"];
+let navProduct = navServices.nextElementSibling;
+navProduct.textContent = siteContent["nav"]["nav-item-2"];
+let navVision = navProduct.nextElementSibling;
+navVision.textContent = siteContent["nav"]["nav-item-3"];
+let navFeatures = navVision.nextElementSibling;
+navFeatures.textContent = siteContent["nav"]["nav-item-4"];
+let navAbout = navFeatures.nextElementSibling;
+navAbout.textContent = siteContent["nav"]["nav-item-5"];
+let navContact = navAbout.nextElementSibling;
+navContact.textContent = siteContent["nav"]["nav-item-6"];
+const bonus = document.createElement('a')
+bonus.href = '#';
+bonus.textContent = 'Bonus';
+const staff = document.createElement('a')
+staff.href = '#';
+staff.textContent = 'Staff';
+navEl.appendChild(bonus);
+navEl.prepend(staff);
+const anchor = document.querySelectorAll('a');
+anchor.forEach((a) => a.style.color = 'green')
+
+//body
+const midHeadline = document.querySelector('h1');
+midHeadline.textContent = siteContent['cta']['h1'];
+const startButton = document.querySelector('button');
+startButton.textContent = siteContent['cta']['button'];
+let midImg = document.querySelector('#cta-img');
+midImg.setAttribute('src', siteContent['cta']['img-src']);
+
+//main-content(top)
+const features = document.querySelector('div.top-content > div.text-content:nth-child(1) > h4');
+features.textContent = siteContent['main-content']['features-h4'];
+const featuresCon = features.nextElementSibling;
+featuresCon.textContent = siteContent['main-content']['features-content'];
+
+const about = document.querySelector('div.top-content > div:nth-child(2) > h4');
+about.textContent = siteContent['main-content']['about-h4'];
+const aboutCon = document.querySelector('div.top-content > div:nth-child(2) > p');
+aboutCon.textContent = siteContent['main-content']['about-content'];
+
+//main-content(bottom)
+const mainMidImg = document.querySelector('#middle-img');
+mainMidImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+const services = document.querySelector('div.bottom-content > div:nth-child(1) > h4');
+services.textContent = siteContent['main-content']['services-h4'];
+const servicesCon = services.nextElementSibling;
+servicesCon.textContent = siteContent['main-content']['services-content'];
+
+const product = document.querySelector('div.bottom-content > div:nth-child(2) > h4');
+product.textContent = siteContent['main-content']['product-h4'];
+const productCon = product.nextElementSibling;
+productCon.textContent = siteContent['main-content']['product-content'];
+
+const vision = document.querySelector('div.bottom-content > div:nth-child(3) > h4');
+vision.textContent = siteContent['main-content']['vision-h4'];
+const visionCon = vision.nextElementSibling;
+visionCon.textContent = siteContent['main-content']['vision-content'];
+
+//contact section
+const contactt = document.querySelector('section.contact > h4');
+contactt.textContent = siteContent['contact']['contact-h4'];
+const contactAddr = contactt.nextElementSibling;
+contactAddr.textContent = siteContent['contact']['address'];
+const contactNum = contactAddr.nextElementSibling;
+contactNum.textContent = siteContent['contact']['phone'];
+const contactEmail = contactNum.nextElementSibling;
+contactEmail.textContent = siteContent['contact']['email'];
+
+//footer
+const foot = document.querySelector('footer > p');
+foot.textContent = siteContent['footer']['copyright'];
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
